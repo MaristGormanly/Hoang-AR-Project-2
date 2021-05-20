ae219f7061aea592a8d4673c3ba3c5f189942772
Timoth Hoang <timothy.hoang1@marist.edu>
2021-05-20 16:24:48 -0400

    Added physics and geometry as well as a tap function to place a ball above a surface

--- Hoang AR Project 2/ViewController.swift
+++ Hoang AR Project 2/ViewController.swift
@@ -38,7 +38,42 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         sceneView.scene = scene
         
-        //let dummyNode = scene.rootNode.childNodes(withName: ")
+        sceneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:))))
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer){
+        let tapLocation = recognizer.location(in: sceneView)
+        let estimatedPlane: ARRaycastQuery.Target = .estimatedPlane
+        let alignment: ARRaycastQuery.TargetAlignment = .any
+        let query: ARRaycastQuery? = sceneView.raycastQuery(from: tapLocation, allowing: estimatedPlane, alignment: alignment)
+
+        if let nonOptQuery: ARRaycastQuery = query {
+
+            let result: [ARRaycastResult] = sceneView.session.raycast(nonOptQuery)
+
+            guard let rayCast: ARRaycastResult = result.first
+            else { return }
+
+            self.insertGeometry(rayCast)
+        }
+    }
+    
+    func insertGeometry(_ result: ARRaycastResult) {
+
+        let ball = SCNSphere(radius: 0.1)
+        let node = SCNNode(geometry: ball)
+
+        // The physicsBody tells SceneKit this geometry should be manipulated by the physics engine
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        node.physicsBody?.mass = 2.0
+        node.physicsBody?.categoryBitMask = 1
+
+        // We insert the geometry slightly above the point the user tapped, so that it drops onto the plane
+        // using the physics engine
+        
+        node.position = SCNVector3(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y + 0.5, result.worldTransform.columns.3.z)
+
+        self.sceneView.scene.rootNode.addChildNode(node)
     }
     
     func createPlaneNode(anchor: ARPlaneAnchor) -> SCNNode {
@@ -55,6 +90,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
         
         planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2, 1, 0, 0)
+        
+        planeNode.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        planeNode.physicsBody?.categoryBitMask = 2
         
         return planeNode
     }
